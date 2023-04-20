c61d710b60fbbcdd4af77e95f892adc52a445186
zahaibalig <zahaibalig@gmail.com>
2023-04-20 23:57:09 +0200

    timer

--- javaScript.js
+++ javaScript.js
@@ -2825,3 +2825,21 @@ console.log(
   new Intl.NumberFormat(navigator.language, options).format(num)
 );
 
+// Timers
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
