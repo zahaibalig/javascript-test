aef45435302e8bc0dc484323a205e4d9041c3547
zahaibalig <zahaibalig@gmail.com>
2023-05-19 23:10:06 +0200

    logger function

--- javaScript.js
+++ javaScript.js
@@ -4293,3 +4293,75 @@ Promise.resolve('Resolved promise 2').then(res => {
 
 console.log('Test end');
 
+/ Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+
+
+///////////////////////////////////////
+// Functions
+function logger() {
+  console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+
